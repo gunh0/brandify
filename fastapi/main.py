012840da28668df168aade0f559e6edadc154a62
Gunho Park <devgun@kakao.com>
2023-08-27 17:42:54 +0900

    fix: mongo connection error

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = FastAPI()
 
 # 데이터베이스와 컬렉션 선택
-client = MongoClient("mongodb://localhost:27017/")  # 로컬호스트 주소와 포트
+client = MongoClient("mongodb://sk-mongo:27017/")  # 로컬호스트 주소와 포트
 db = client.sk  # 데이터베이스 이름
 mood_collection = db.mood  # mood 컬렉션
 purpose_collection = db.purpose  # purpose 컬렉션
