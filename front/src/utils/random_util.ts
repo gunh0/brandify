499586a3f2dd314a2d56c551cd798963fba7b7bf
Yejin <orobos654@gmail.com>
2023-09-02 22:13:17 +0900

    feat(front): scatter cards

--- front/src/utils/random_util.ts
+++ front/src/utils/random_util.ts
@@ -1,5 +1,8 @@
 import {Rect} from './rect_util.ts';
 
+const colScatterDegree = 80;
+const rowScatterDegree = 40;
+
 export const random = (arr: Rect[], vw: number, vh: number) => {
   const totalCol = Math.ceil(Math.sqrt(arr.length));
   const totalRow = Math.ceil(arr.length / totalCol);
@@ -8,7 +11,10 @@ export const random = (arr: Rect[], vw: number, vh: number) => {
   arr.forEach((rect, idx) => {
     const col = idx % totalCol,
       row = Math.floor(idx / totalCol);
-    rect.top = row * rowHeight;
-    rect.left = col * colWidth;
+    rect.top = Math.min(
+      Math.max(rowHeight / 5, row * rowHeight + rowScatterDegree * 2 * Math.random() - rowScatterDegree),
+      vh - rowHeight / 5,
+    );
+    rect.left = Math.max(colWidth / 5, col * colWidth + colScatterDegree * 2 * Math.random() - colScatterDegree);
   });
 };
